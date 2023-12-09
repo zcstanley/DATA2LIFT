90172f358834c7405298b29818d70174f31f07a1
Zofia Stanley <46766742+zcstanley@users.noreply.github.com>
2023-12-09 09:01:33 -0800

    removed data download from makeDisconnectedMap.R

--- disconnected/Scripts/makeDisconnectedMap.R
+++ disconnected/Scripts/makeDisconnectedMap.R
@@ -4,9 +4,6 @@ library(pbmcapply)  # parallel version of lapply
 library(sf)
 library(cartogram)
 
-# Set census api key
-census_api_key("d48d2c6fa999265383738be2b1f48aed4900866a")
-
 # Set your states
 states_list <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
                  "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
@@ -14,37 +11,7 @@ states_list <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
                  "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
                  "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC")
 
-# Function to get data for a specific state with retries on failure
-getDataByState <- function(state, year, max_retries = 3) {
-  # Define the required variables from ACS
-  vars <- c("AGEP", "SCH", "ESR", "RAC1P", "HISP", "SEX", "ST", "PUMA")
-  
-  retries <- 1
-  
-  while (retries <= max_retries) {
-    tryCatch({
-      # Retrieve the data for the given year and state with specified variables
-      data <- get_pums(variables = vars, year = year, state = state, survey = "acs1") %>%
-        filter(AGEP >= 16 & AGEP <= 24, SCH == 1, ESR %in% c(3, 6))  # Filtering conditions
-      return(data)
-    }, error = function(e) {
-      if (retries == max_retries) {
-        warning(paste("Fetching data for state", state, "failed after", max_retries, "attempts. Returning empty data frame."))
-        return(data.frame())  # Return an empty data frame
-      }
-      retries <- retries + 1
-    })
-  }
-}
-
-# Use pbmclapply to fetch data in parallel
-rawdata_list <- pbmclapply(states_list, function(state) {
-  getDataByState(state, 2021)
-}, mc.cores = 23)
-
-# Combine all the data into one data frame
-# rawdata <- bind_rows(rawdata_list)
-# save(rawdata, file = "disconnected/RawData/disconnectedRawData.Rdata") 
+# Load raw data (if it exists), else prompt user to download data using downloadDisconnectedData.R
 load("disconnected/RawData/disconnectedRawData.Rdata")
 
 # Recode data for clarity
@@ -103,7 +70,7 @@ p2 <- plotDisconnected(alaska)
 p3 <- plotDisconnected(hawaii)
 
 
-# Save the plot as a PDF file
+# Save the plots as PDF files
 ggsave(filename = "disconnected/Plots/disconnectedContiguousUS2021Map.pdf", plot = p1, width = 10, height = 6)
 ggsave(filename = "disconnected/Plots/disconnectedAlaska2021Map.pdf", plot = p2, width = 10, height = 6)
 ggsave(filename = "disconnected/Plots/disconnectedHawaii2021Map.pdf", plot = p3, width = 10, height = 6)
